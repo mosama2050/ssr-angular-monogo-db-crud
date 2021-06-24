a0c5aed8f5918f92c5fc5179eee968d51a0e375a
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-06-24 14:51:00 +0200

    book add & edit

--- src/app/books-add/books-add.component.ts
+++ src/app/books-add/books-add.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-books-add',
@@ -7,9 +19,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksAddComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+  isbn = '';
+  book = '';
+  author = '';
+  description = '';
+  publisher = '';
+  publishedYear = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.bookForm = this.formBuilder.group({
+      'isbn' : [null, Validators.required],
+      'title' : [null, Validators.required],
+      'author' : [null, Validators.required],
+      'description' : [null, Validators.required],
+      'publisher' : [null, Validators.required],
+      'publishedYear' : [null, Validators.required]
+    });
   }
+
+  onFormSubmit(): void {
+    this.isLoadingResults = true;
+    this.api.addBook(this.bookForm.value)
+      .subscribe((res: any) => {
+        const id = res._id;
+        this.isLoadingResults = false;
+        this.router.navigate(['/book-details', id]);
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
+  }
+
 
 }
