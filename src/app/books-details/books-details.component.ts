62bb730376c05426c2fe90ad381fecfc28621d40
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-06-24 14:31:20 +0200

    book details

--- src/app/books-details/books-details.component.ts
+++ src/app/books-details/books-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-books-details',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksDetailsComponent implements OnInit {
 
-  constructor() { }
+
+  // @ts-ignore
+  book: Book = {
+    _id: '',
+    isbn: '',
+    title: '',
+    author: '',
+    description: '',
+    publisher: '',
+    publishedYear: '',
+    // @ts-ignore
+    updatedAt: null
+  };
+  isLoadingResults = true;
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getBookDetails(this.route.snapshot.paramMap.get('id'));
   }
 
+  getBookDetails(id: any): void {
+    this.api.getBook(id)
+      .subscribe((data: any) => {
+        this.book = data;
+        console.log(this.book);
+        this.isLoadingResults = false;
+      });
+  }
+
+  deleteBook(id: any): void {
+    this.isLoadingResults = true;
+    this.api.deleteBook(id)
+      .subscribe(() => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/books']);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
 }
